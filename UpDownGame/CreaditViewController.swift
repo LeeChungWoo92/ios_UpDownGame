83c67b245ce910fb9a865907f7011d64c3f8bce2
LeeChungWoo <skcjddn@gmail.com>
2022-04-26 00:38:12 +0900

    Add: CreaditViewController

---
+++ UpDownGame/CreaditViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreaditViewController.swift
+//  UpDownGame
+//
+//  Created by chungwoolee on 2022/04/25.
+//
+
+import UIKit
+
+class CreaditViewController: UIViewController {
+    
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton){
+        dismiss(animated: true,
+                completion: nil)
+    }
+    
+    
+}
